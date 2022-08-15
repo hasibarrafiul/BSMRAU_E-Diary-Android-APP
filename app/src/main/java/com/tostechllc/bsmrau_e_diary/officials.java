7ae3ba84ab98bca9847ff94e16f9c04176e28b92
Rashik Buksh Rafsan <85847421+rashikbuksh@users.noreply.github.com>
2022-08-16 00:38:15 +0600

    fetch officials added

--- app/src/main/java/com/tostechllc/bsmrau_e_diary/officials.java
+++ app/src/main/java/com/tostechllc/bsmrau_e_diary/officials.java
@@ -2,13 +2,86 @@ package com.tostechllc.bsmrau_e_diary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class officials extends AppCompatActivity {
+
+    public static final String FETCH_OFFICIALS = "https://tostechllc.com/android/getOfficials.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_officials);
+
+        fetchFaculty();
+    }
+    public void fetchFaculty(){
+        @SuppressLint("StaticFieldLeak")
+        class dbManager extends AsyncTask<String,Void,String>
+        {
+            protected void onPostExecute(String data){
+                try {
+                    JSONArray ja = new JSONArray(data);
+                    JSONObject jo = null;
+
+                    for(int i =0;i<ja.length();i++){
+                        jo=ja.getJSONObject(i);
+                        int officialid = jo.getInt("id");
+                        String name = jo.getString("name");
+                        String designation = jo.getString("designation");
+                        String department = jo.getString("department");
+                        int mobilenumber = jo.getInt("mobilenumber");
+                        String email = jo.getString("email");
+                        int officenumber = jo.getInt("officenumber");
+                        String image = jo.getString("image");
+                        int status = jo.getInt("status");
+
+                        System.out.println(officialid+" "+name+" "+department);
+
+                        //postedDirectory postedDirectorysingle = new postedDirectory(directoryid,catagory,department);
+
+                        //System.out.println("directory: "+postedDirectorysingle.id);
+
+                        //arrayList.add(postedDirectorysingle);
+
+                        //loadDatainList();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            protected String doInBackground(String... strings) {
+                try {
+                    URL url = new URL(strings[0]);
+                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                    StringBuffer data = new StringBuffer();
+                    String line;
+
+                    while((line=br.readLine())!=null){
+                        data.append(line+"\n");
+                    }
+                    br.close();
+                    return data.toString();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }
+        dbManager obj =new dbManager();
+        obj.execute(FETCH_OFFICIALS);
     }
 }
