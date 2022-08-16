c4fd9019ae3109b49cdffed44ff530938317e359
Shahadat Anik <git.shahadat.anik@gmail.com>
2022-08-17 00:11:54 +0600

    Design Homepage, Directory and Custom views
    - Everything needs to be combine

--- app/src/main/java/com/tostechllc/bsmrau_e_diary/home.java
+++ app/src/main/java/com/tostechllc/bsmrau_e_diary/home.java
@@ -1,71 +1,15 @@
 package com.tostechllc.bsmrau_e_diary;
 
-import android.annotation.SuppressLint;
-import android.app.AlertDialog;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.style.UnderlineSpan;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.SubMenu;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
 
 
+public class home extends AppCompatActivity  {
 
-public class home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
-    ImageButton Menu;
-    private AppBarConfiguration mAppBarConfiguration;
-
-    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        Menu = findViewById(R.id.menu);
-        Menu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                drawer.openDrawer(Gravity.RIGHT);
-            }
-        });
-
-//        findViewById(R.id.menu).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                drawer.openDrawer(Gravity.RIGHT);
-//            }
-//        });
-
-
-    }
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
 
     }
 
