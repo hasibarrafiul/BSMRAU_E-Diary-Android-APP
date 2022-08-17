bb005f95c82f442dbd485aaf8a448cb97af49d3e
Rashik Buksh Rafsan <85847421+rashikbuksh@users.noreply.github.com>
2022-08-17 23:49:34 +0600

    faculty and officials all data can be fetched.

--- app/src/main/java/com/tostechllc/bsmrau_e_diary/listedFaculty.java
+++ app/src/main/java/com/tostechllc/bsmrau_e_diary/listedFaculty.java
@@ -1,8 +1,8 @@
 package com.tostechllc.bsmrau_e_diary;
 
 public class listedFaculty {
-    int id;
-    String name, department;
+    int id, mobilenumber, officenumber, status;
+    String name, designation, department, email, image;
 
     public listedFaculty(int id , String name , String department) {
         this.id = id;
@@ -10,6 +10,67 @@ public class listedFaculty {
         this.department = department;
 
     }
+
+    public listedFaculty(int id, int mobilenumber, int officenumber, int status, String name, String designation, String department, String email, String image) {
+        this.id = id;
+        this.mobilenumber = mobilenumber;
+        this.officenumber = officenumber;
+        this.status = status;
+        this.name = name;
+        this.designation = designation;
+        this.department = department;
+        this.email = email;
+        this.image = image;
+    }
+
+    public int getMobilenumber() {
+        return mobilenumber;
+    }
+
+    public void setMobilenumber(int mobilenumber) {
+        this.mobilenumber = mobilenumber;
+    }
+
+    public int getOfficenumber() {
+        return officenumber;
+    }
+
+    public void setOfficenumber(int officenumber) {
+        this.officenumber = officenumber;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
     public listedFaculty(){}
 
     public int getId() {
