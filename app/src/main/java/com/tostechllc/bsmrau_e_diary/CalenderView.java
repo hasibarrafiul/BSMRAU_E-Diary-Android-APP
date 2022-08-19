db4c94c533190525058fb84ed5fe0568599d75e6
gl1tchy_af <fahad.com66.fk@gmail.com>
2022-08-20 00:42:59 +0600

    govment holiday added

--- app/src/main/java/com/tostechllc/bsmrau_e_diary/CalenderView.java
+++ app/src/main/java/com/tostechllc/bsmrau_e_diary/CalenderView.java
@@ -75,11 +75,52 @@ public class CalenderView extends AppCompatActivity {
                         String datetime = date.getString("datetime");
                         JSONObject getDay = new JSONObject(datetime);
                         String day = getDay.getString("day");
-                        System.out.println(holidayName);
-                        System.out.println(holidayDesc);
-                        System.out.println(day);
+                        String month = getDay.getString("month");
 
-                        holidayListView holidayListView = new holidayListView(holidayName, holidayDesc, day);
+//                        System.out.println(holidayName);
+//                        System.out.println(holidayDesc);
+//                        System.out.println(day);
+                        switch(month){
+                            case "1":
+                                month = "JAN";
+                                break;
+                            case "2":
+                                month = "FEB";
+                                break;
+                            case "3":
+                                month = "MAR";
+                                break;
+                            case "4":
+                                month = "APR";
+                                break;
+                            case "5":
+                                month = "MAY";
+                                break;
+                            case "6":
+                                month = "JUN";
+                                break;
+                            case "7":
+                                month = "JUL";
+                                break;
+                            case "8":
+                                month = "AUG";
+                                break;
+                            case "9":
+                                month = "SEP";
+                                break;
+                            case "10":
+                                month = "OCT";
+                                break;
+                            case "11":
+                                month = "NOV";
+                                break;
+                            case "12":
+                                month = "DEC";
+                                break;
+                        }
+
+
+                        holidayListView holidayListView = new holidayListView(holidayName, holidayDesc, month+" "+day);
                         arrayListHoliday.add(holidayListView);
                         loadDatainList();
 
